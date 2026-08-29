eafb96688cbff46ed1b8b764befbad158114fef5
Waqqas Sheikh <waqqas.abdulkareem@gmail.com>
2026-08-29 14:58:27 +0400

    Initial Commit. Mean HR Agent

---
+++ README.md
@@ -0,0 +1,48 @@
+# Langfuse Experiment
+
+## Steps to run 
+
+1. Install dependencies
+
+    ```shell
+    uv sync
+    ```
+
+2. Copy .env with your Langfuse credentials
+
+3. This application expects llama2:7b to be running on ollama.
+
+    ```shell
+    ollama run llama2:7b
+    ```
+
+4. Publish the hr_agent prompt
+
+    ```shell
+    uv run main.py prompts --publish hr_agent
+    ```
+
+5. Chat with the agent
+
+    ```
+    uv run main.py chat
+    ```
+
+![Screenshot](./docs/Screenshot.png)
+
+## To Do
+
+- [x] Setup Ollama with gemma / falcon
+- [x] Prompt gemma, falcon from langchain
+- [x] Setup chat memory
+- [x] Get the system prompt from SaaS langfuse
+- [ ] Setup Evals in the CI/CD pipeline -OR- Experimentation on Langfuse
+- [ ] Generate a generic hr handbook and set up rag over the handbook. 
+- [ ] Set up evals on language to monitor kpis
+    1. Agent groundedness
+    2. Token used
+    3. Time to respond
+
+## Useful Resources
+
+- [Prompt Managment] https://langfuse.com/docs/prompt-management/get-started
---
+++ main.py
@@ -0,0 +1,81 @@
+from dataclasses import dataclass
+import sys
+import argparse
+from dotenv import load_dotenv
+from langchain.chat_models import BaseChatModel
+from langfuse import Langfuse, get_client
+from langchain_ollama import ChatOllama
+from langgraph.runtime import Runtime
+from langgraph.graph import END, START, MessagesState, StateGraph
+from langgraph.checkpoint.memory import InMemorySaver
+from langchain_core.runnables import RunnableConfig
+from langchain.messages import HumanMessage, SystemMessage
+from prompt_ops import publish_prompt
+
+
+@dataclass
+class Deps:
+    prompt_registry: Langfuse
+    llm: BaseChatModel
+
+
+def call_model(state: MessagesState, runtime: Runtime[Deps]):
+    prompt_client = runtime.context.prompt_registry.get_prompt("hr_agent")
+    system_prompt = prompt_client.compile()
+    ai_msg = runtime.context.llm.invoke([system_prompt] + state["messages"])
+    return {"messages": [ai_msg]}
+
+
+def chat(langfuse):
+    llm = ChatOllama(
+        model="llama2:7b",
+        temperature=0,
+    )
+
+    workflow = StateGraph(MessagesState, context_schema=Deps)
+    workflow.add_node(call_model)
+    workflow.add_edge(START, "call_model")
+    workflow.add_edge("call_model", END)
+
+    checkpointer = InMemorySaver()
+    graph = workflow.compile(checkpointer=checkpointer)
+
+    config: RunnableConfig = {"configurable": {"thread_id": "1"}}
+
+    while True:
+        user_input = input("Human: ")
+
+        if user_input.lower() == "q":
+            print("Bye")
+            break
+
+        result = graph.invoke(
+            {"messages": [HumanMessage(user_input)]},
+            config,
+            context=Deps(langfuse, llm),
+        )
+        print("AI: {}".format(result["messages"][-1].content))
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    langfuse = get_client()
+
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest="command", required=True)
+
+    prompts_subparser = subparsers.add_parser("prompts", help="Prompt registry actions")
+    prompts_subparser.add_argument("-p", "--publish", help="Publish prompt to registry")
+
+    chat_subparser = subparsers.add_parser("chat", help="Chat with agent")
+
+    args = parser.parse_args()
+
+    if args.command == "chat":
+        chat(langfuse)
+    elif args.command == "prompts":
+        if args.publish:
+            publish_prompt(langfuse, args.publish)
+    else:
+        parser.print_help()
+        sys.exit(1)
